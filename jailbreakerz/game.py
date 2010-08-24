6c0531a8f3043aee3f8dc2d668b1b2eb22bbfbad
Flávio Ribeiro <email@flavioribeiro.com>
2010-08-24 15:15:14 -0300

    Add a falling thief

--- jailbreakerz/game.py
+++ jailbreakerz/game.py
@@ -1,51 +1,69 @@
-import cocos
+from cocos.sprite import *
+from cocos.director import director
+from cocos.layer import Layer
+from cocos.scene import Scene
+from cocos.scenes.transitions import *
+from cocos.actions import *
+from cocos.sprite import *
+from cocos.menu import *
+from cocos.text import *
+from pyglet import font
+
 import pyglet
 
-class Game(cocos.layer.Layer):
+class Game(Layer):
 
     def __init__(self):
         super(Game, self).__init__()
         self.load_sprites()
 
     def load_sprites(self):
-        self.prison = cocos.sprite.Sprite('media/imgs/prison.png')
+        self.prison = Sprite('media/imgs/prison.png')
         self.prison.position = 100,170
         self.add(self.prison)
 
-        self.thief = cocos.sprite.Sprite('media/imgs/tall_thief.png')
-        self.thief.position = 250,440
-        self.add(self.thief)
-
-        self.kombi = cocos.sprite.Sprite('media/imgs/kombi.png')
-        self.kombi.position = 750, 120
+        self.kombi = Sprite('media/imgs/kombi.png')
+        self.kombi.position = 750,120
         self.add(self.kombi)
 
-class Catcher(cocos.layer.Layer):
-    
+class FallingThief(Layer):
+
+    def __init__(self, *args, **kwargs):
+        super(FallingThief, self).__init__()
+        self.thief = Sprite('media/imgs/tall_thief.png')
+        self.thief.position = 100, 190
+        self.add(self.thief)
+        self.fall()
+
+    def fall(self):
+        action = JumpBy((500,0), height=100, jumps=10, duration=7)
+        self.thief.do(action)
+
+class Catcher(Layer):
+
     is_event_handler = True
-    
-    MOVEMENT_RATE = 100  # Constant used to move sprite
-    
+
+    MOVEMENT_RATE = 50  # Constant used to move sprite
+
     def __init__(self, *args, **kwargs):
         super(Catcher, self).__init__()
-        self.catcher = cocos.sprite.Sprite('media/imgs/catcher.png')
+        self.catcher = Sprite('media/imgs/catcher.png')
         self.catcher.position = 300,100
         self.add(self.catcher)
-        
+
     def on_key_press(self, key, modifiers):
         """
         Catcher movement
-        
         @param key: int
         """
         if key == pyglet.window.key.LEFT:
             self.catcher.position = self.catcher.position[0] - self.MOVEMENT_RATE, self.catcher.position[1]
         elif key == pyglet.window.key.RIGHT:
-            self.catcher.position = self.catcher.position[0] + self.MOVEMENT_RATE, self.catcher.position[1]            
+            self.catcher.position = self.catcher.position[0] + self.MOVEMENT_RATE, self.catcher.position[1]
 
 
 if __name__ == '__main__':
-    cocos.director.director.init(resizable=False, width=800, height=600)
+    director.init(resizable=False, width=800, height=600)
 
-    scene = cocos.scene.Scene(Game(), Catcher())
-    cocos.director.director.run(scene)
+    scene = Scene(Game(), Catcher(), FallingThief())
+    director.run(scene)
